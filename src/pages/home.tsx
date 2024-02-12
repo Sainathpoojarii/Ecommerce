09d1d7fd39ab23e5dd4d20c45063fe59ad6feb3e
sainath <sainathpoojari19@gmail.com>
2024-02-12 02:09:36 -0800

    header done

---
+++ src/pages/home.tsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div className="home">
+      <section>
+
+      </section>
+
+<h1>Latest Product
+  <Link to={"/search"} className="findmore" >
+    More
+  </Link>
+</h1>
+
+<main>
+  
+</main>
+    </div>
+  )
+}
+
+export default Home
